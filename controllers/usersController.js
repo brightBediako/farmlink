c7f2e4ba766eb845270aca59d27a22b4e9b617c9
Bright Bediako <bright.bediako.dev@gmail.com>
2025-10-14 13:18:06 +0000

    modify files

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -16,73 +16,73 @@ import Product from "../models/Product.js";
 // desc    register
 // route   POST /api/v1/users/register
 // access  Private/Admin
-export const registerUserController = asyncHandler(async (req, res) => {
-  const { fullname, email, phone, password, role } = req.body;
+// export const registerUserController = asyncHandler(async (req, res) => {
+//   const { fullname, email, phone, password, role } = req.body;
 
-  // check if user exist
-  const userExists = await User.findOne({ email });
-  if (userExists) {
-    throw new Error("User already Exists");
-    return;
-  }
-  // hash password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  // create user
-  const user = await User.create({
-    fullname,
-    email,
-    phone,
-    password: hashedPassword,
-    role: role || "buyer",
-  });
+//   // check if user exist
+//   const userExists = await User.findOne({ email });
+//   if (userExists) {
+//     throw new Error("User already Exists");
+//     return;
+//   }
+//   // hash password
+//   const salt = await bcrypt.genSalt(10);
+//   const hashedPassword = await bcrypt.hash(password, salt);
+//   // create user
+//   const user = await User.create({
+//     fullname,
+//     email,
+//     phone,
+//     password: hashedPassword,
+//     role: role || "buyer",
+//   });
 
-  // create notification
-  const notification = await Notification.create({
-    userId: user._id,
-    message: `<p>
-  Welcome to <strong>FarmLink</strong> – your trusted platform for buying and selling fresh farm produce!
-</p>`,
-  });
-  if (user && user.email) {
-    await sendRegisterNotificationEmail(user.email, user.fullname);
-  }
+//   // create notification
+//   const notification = await Notification.create({
+//     userId: user._id,
+//     message: `<p>
+//   Welcome to <strong>FarmLink</strong> – your trusted platform for buying and selling fresh farm produce!
+// </p>`,
+//   });
+//   if (user && user.email) {
+//     await sendRegisterNotificationEmail(user.email, user.fullname);
+//   }
 
-  res.status(201).json({
-    status: "success",
-    message: "User Registered Successfully",
-    data: user,
-  });
-});
+//   res.status(201).json({
+//     status: "success",
+//     message: "User Registered Successfully",
+//     data: user,
+//   });
+// });
 
-// desc    login
-// route   POST /api/v1/users/login
-// access  Public
+// // desc    login
+// // route   POST /api/v1/users/login
+// // access  Public
 
-export const loginUserController = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-  // check if user exist in db
-  const userFound = await User.findOne({
-    email,
-  });
-  if (userFound && (await bcrypt.compare(password, userFound?.password))) {
-    res.json({
-      status: "success",
-      message: "Login Successfully",
-      user: {
-        _id: userFound._id,
-        fullname: userFound.fullname,
-        email: userFound.email,
-        phone: userFound.phone,
-        role: userFound.role,
-        // add other fields as needed
-      },
-      token: generateToken(userFound?._id),
-    });
-  } else {
-    throw new Error("Invalid Login Credentials");
-  }
-});
+// export const loginUserController = asyncHandler(async (req, res) => {
+//   const { email, password } = req.body;
+//   // check if user exist in db
+//   const userFound = await User.findOne({
+//     email,
+//   });
+//   if (userFound && (await bcrypt.compare(password, userFound?.password))) {
+//     res.json({
+//       status: "success",
+//       message: "Login Successfully",
+//       user: {
+//         _id: userFound._id,
+//         fullname: userFound.fullname,
+//         email: userFound.email,
+//         phone: userFound.phone,
+//         role: userFound.role,
+//         // add other fields as needed
+//       },
+//       token: generateToken(userFound?._id),
+//     });
+//   } else {
+//     throw new Error("Invalid Login Credentials");
+//   }
+// });
 
 // desc    get user
 // route   POST /api/v1/users/profile
@@ -314,7 +314,8 @@ export const verifyEmailTokenController = asyncHandler(async (req, res) => {
   res.json({
     token,
     status: "success",
-    message:"Email verification token sent to your email and expires in 10 minutes",
+    message:
+      "Email verification token sent to your email and expires in 10 minutes",
   });
 });
 

77625ff1fbd29d876436ad98f778367dd70c3233
Bright Bediako <bright.bediako.dev@gmail.com>
2025-11-18 06:30:26 +0000

    modify files

--- backend/routes/brandsRoute.js
+++ backend/routes/brandsRoute.js
@@ -1,10 +1,10 @@
-import exppress from "express";
+import express from "express";
 import {
-    createBrandController,
-    deleteBrandController,
-    getAllBrandsController,
-    getSingleBrandController,
-    updateBrandController,
+  createBrandController,
+  deleteBrandController,
+  getAllBrandsController,
+  getSingleBrandController,
+  updateBrandController,
 } from "../controllers/brandsController.js";
 import { isLoggedIn } from "../middleware/isLoggedIn.js";
 import isAdmin from "../middleware/isAdmin.js";
@@ -15,6 +15,11 @@ brandsRoute.post("/", isLoggedIn, isAdmin, createBrandController);
 brandsRoute.get("/", getAllBrandsController);
 brandsRoute.get("/:id", getSingleBrandController);
 brandsRoute.delete("/:id", isLoggedIn, isAdmin, deleteBrandController);
-brandsRoute.put("/update-brand/:id", isLoggedIn, isAdmin, updateBrandController);
+brandsRoute.put(
+  "/update-brand/:id",
+  isLoggedIn,
+  isAdmin,
+  updateBrandController
+);
 
 export default brandsRoute;
